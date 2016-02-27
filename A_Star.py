953c2f342bca7da2ea43e4e70d6d351f6e163d6f
Wojciech <wojciech.bwa@gmail.com>
2016-02-27 18:22:37 +0100

    Added continuous obstacles marking. Bug fixes. Dodano ciagle zaznaczanie przeszkod. Poprawiono bledy.

--- A_Star.py
+++ A_Star.py
@@ -14,13 +14,17 @@ class Block:
         self.__end_coords = end_coords
 
         if end_coords:
-            self.__H = 4 * min(abs(end_coords[0] - x), abs(end_coords[1] - y)) +\
-                       10 * max(abs(end_coords[0] - x), abs(end_coords[1] - y))
+            self.__H = 4 * min(abs(end_coords[0] - x),\
+                               abs(end_coords[1] - y)) +\
+                10 * max(abs(end_coords[0] - x),\
+                         abs(end_coords[1] - y))
         else:
             self.__H = 0
 
         if parent:
-            self.__G = parent.getG() + int(10 * ((parent.getY() - y) ** 2 + (parent.getX() - x) ** 2) ** (1/2))
+            self.__G = parent.getG() +\
+                       int(10 * ((parent.getY() - y) ** 2 +\
+                                 (parent.getX() - x) ** 2) ** (1/2))
         else:
             self.__G = 0
 
@@ -35,8 +39,9 @@ class Block:
 
     def setParent(self, parent):
         self.__parent = parent
-        self.__G = int(10 * ((parent.getY() - self.getY()) ** 2 + (parent.getX() - self.getX()) ** 2) ** (1/2)) +\
-                   parent.getG()
+        self.__G = parent.getG() +\
+                   int(10 * ((parent.getY() - self.getY()) ** 2 +\
+                             (parent.getX() - self.getX()) ** 2) ** (1/2))
 
     def getX(self):
         return self.__coords[0]
@@ -55,7 +60,9 @@ class Block:
 
 class Board(tk.Canvas):
     def __init__(self, master, width, height, block_size):
-        tk.Canvas.__init__(self, master, width=width*block_size, height=height*block_size)
+        tk.Canvas.__init__(self, master,\
+                           width=width*block_size,\
+                           height=height*block_size)
         self.__width = block_size * width
         self.__height = block_size * height
         self.__size = block_size
@@ -67,13 +74,20 @@ class Board(tk.Canvas):
         border = 1 if block_size > 10 else 0
         for i in range(width):
             for j in range(height):
-                self.__grid.append(self.create_rectangle(i * block_size + border, j * block_size + border,\
-                                                         (i+1) * block_size - border, (j+1) * block_size - border))
+                self.__grid.append(self.create_rectangle(\
+                                    i * block_size + border,\
+                                    j * block_size + border,\
+                                    (i+1) * block_size - border,\
+                                    (j+1) * block_size - border))
 
     def mark_block(self, coords, state):
-        color_dict = {"open": "orange1", "closed": "orange4", "path": "cyan", "obstacle": "gray", "start": "green", "end": "red"}
-        tag = int(self.__height / self.__size) * coords[0] + coords[1] + 1
-        self.itemconfig(tag, fill=color_dict[state])
+        color_dict = {"open": "orange1", "closed": "orange4",\
+                      "path": "cyan", "obstacle": "gray",\
+                      "start": "green", "end": "red"}
+        if 0 <= coords[0] < self.__width / self.__size and\
+           0 <= coords[1] < self.__height / self.__size:
+            tag = int(self.__height / self.__size) * coords[0] + coords[1] + 1
+            self.itemconfig(tag, fill=color_dict[state])
 
     def create_point(self, x, y):
         return tk.Canvas.create_line(self, x, y, x+1, y+1)
@@ -103,7 +117,7 @@ class Board(tk.Canvas):
         return self.__end_coords
 
     def setObstacle(self, coords, convert=False):
-        coords = (coords[0], coords[1])                                          
+        coords = (coords[0], coords[1])                                        
         if convert:
             coords = self.board_coords(coords)
         self.mark_block(coords, "obstacle")
@@ -116,16 +130,19 @@ class Board(tk.Canvas):
         return self.__size
 
     def board_coords(self, canvas_coords):
-        return (int(canvas_coords[0] / self.__size), int(canvas_coords[1] / self.__size))
+        return (int(canvas_coords[0] / self.__size),\
+                int(canvas_coords[1] / self.__size))
 
     def canvas_coords(self, board_coords):
         canvas_coords = []
         for coords in board_coords:
-            canvas_coords.append((coords[0] * self.__size, coords[1] * self.__size))
+            canvas_coords.append((coords[0] * self.__size,\
+                                  coords[1] * self.__size))
         return canvas_coords
 
     def getDims(self):
-        return (int(self.__width / self.__size), int(self.__height / self.__size))
+        return (int(self.__width / self.__size),\
+                int(self.__height / self.__size))
 
 
 def A_Star(board):
@@ -159,7 +176,7 @@ def A_Star(board):
                     (x, y) not in obstacles_coords and\
                     (x, y-dy) not in obstacles_coords and\
                     (x-dx, y) not in obstacles_coords and\
-                    0 <= x <= board_width and 0 <= y <= board_height-1:
+                    0 <= x < board_width and 0 <= y < board_height:
 
                     new_block = Block(x, y, current, end_coords)
                     new_coords = (x, y)
@@ -205,12 +222,13 @@ def A_Star(board):
                 if (x, y) in open_coords and\
                    (x, y-dy) not in obstacles_coords and\
                    (x-dx, y) not in obstacles_coords and\
-                0 <= x <= board_width and 0 <= y <= board_height-1:
+                0 <= x < board_width and 0 <= y < board_height:
 
                     index = open_coords.index((x, y))
                     block = open_blocks[index]
 
-                    if block.getG() > current.getG() + int(10 * (dx ** 2 + dy ** 2) ** (1/2)):
+                    if block.getG() > current.getG() +\
+                       int(10 * (dx ** 2 + dy ** 2) ** (1/2)):
                         block.setParent(current)
 
                         # if board.getSize() >= 30:
@@ -272,7 +290,7 @@ def wrap_setBlocks(board):
                 board.setStart(x, y)
             elif not board.getEndCoords():
                 board.setEndCoords(x, y)
-            else:
+            elif board.board_coords((x, y)) not in board.getObstacles():
                 board.setObstacle((x, y))
 
     return setBlocks
@@ -280,9 +298,10 @@ def wrap_setBlocks(board):
 if __name__ == "__main__":
 
     root = tk.Tk()
-    board = Board(root, 40, 30, 10)
+    board = Board(root, 80, 60, 10)
     board.pack()
     mark_func = wrap_setBlocks(board)
+    board.bind("<B1-Motion>", mark_func)
     board.bind("<Button-1>", mark_func)
 
     button = tk.Button(root, text="go", command=start(board))
